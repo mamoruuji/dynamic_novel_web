744cb8db11d07ffd5b5cbc4ed3d5987309639f98
uji <uji@idiot-savant.co.jp>
2023-01-04 17:43:09 +0000

    画面作成（MUIライブラリによるサイドバー実装方法調査）

--- next.config.js
+++ next.config.js
@@ -5,6 +5,29 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    novelDetailItem: [
+      "タイトル",
+    ],
+    searchOrderItem: [
+      "タイトル",
+    ],
+    SNSlist: [
+      "Twitter",
+    ],
+    readerFunclist: [
+      "閲覧履歴",
+      "お気に入り一覧",
+      "メモ一覧",
+      "感想一覧",
+    ],
+    createrFunclist: [
+      "自分の作品",
+      "パーツ登録",
+      "グループ編集",
+      "パレット追加",
+    ],
+  }
 }
 
 module.exports = nextConfig
