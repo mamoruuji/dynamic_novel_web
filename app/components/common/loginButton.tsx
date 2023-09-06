39c3943900b4908853aad4f41bed7d966627f85e
uji <uji@idiot-savant.co.jp>
2023-09-06 03:55:53 +0000

    ログイン認証実装（パスワードレス認証は別の機会に実装予定）

---
+++ app/components/common/loginButton.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function LoginButton() {
+
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <img
+          src={session.user.image}
+          alt="icon"
+          style={{ width: "100px", height: "100px" }}
+        />
+        <button style={{marginRight: 10}} onClick={() => signOut()}>
+          Sign out
+        </button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button style={{marginRight: 10}} onClick={() => signIn()}>
+        Sign in
+      </button>
+      <button style={{marginRight: 10}} onClick={() => signIn("google")}>
+        Sign in with google
+      </button>
+      <button style={{marginRight: 10}} onClick={() => signIn("github")}>
+        Sign in with github
+      </button>
+    </>
+  )
+}
