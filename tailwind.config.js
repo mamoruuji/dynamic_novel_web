c0fc9bd58232f276b617b520a34ac0191b86bd87
uji <uji@idiot-savant.co.jp>
2024-01-15 07:51:14 +0000

    ボタンの色表示の修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,8 @@ module.exports = {
   theme: {
     extend: {},
   },
+  corePlugins: {
+    preflight: false, // リセットCSSの無効化
+  },
+ important: '#__next', // 全てのクラスセレクタの先頭に '#app' または '#__next'を追加する
 }
